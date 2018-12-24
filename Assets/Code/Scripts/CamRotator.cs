14a4e78c00cd78d7507d8ea27669d2807b7d376e
DESKTOP-R1B44U8\admin <cyberyock@gmail.com>
2018-12-24 16:15:29 +0700

    Rotation Camera

---
+++ Assets/Code/Scripts/CamRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotator : MonoBehaviour {
+
+    public float rotateSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
